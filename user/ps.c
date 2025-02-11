0252d7754c2152a19da76522e04025816f7101c7
Nishil <nishilkapadia17@gmail.com>
2025-02-11 16:32:51 -0500

    user feature completed

--- user/ps.c
+++ user/ps.c
@@ -1,11 +1,134 @@
+#include "kernel/param.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "kernel/fcntl.h"
 #include "user/user.h"
+#include "kernel/fcntl.h"
+#include "kernel/spinlock.h"
+#include "kernel/riscv.h"
+#include "kernel/proc.h"
+
+#define MAXCOLS 5
+
+static char *states[] = {
+  [0] = "UNUSED",
+  [1] = "USED",
+  [2] = "SLEEP",
+  [3] = "RUNBLE",
+  [4] = "RUN",
+  [5] = "ZOMBIE",
+};
+
+static int
+parse_pids(char *str, int pids[], int max_count)
+{
+  int count = 0;
+  char *p = str;
+  while(*p && count < max_count){
+    pids[count++] = atoi(p);
+    char *comma = strchr(p, ',');
+    if(!comma)
+      break;
+    p = comma + 1;
+  }
+  return count;
+}
 
 int
 main(int argc, char *argv[])
 {
-  ps();
+  int pids[32];
+  int pid_count = 0;
+
+  char *columns[MAXCOLS];
+  int num_cols = 0;
+
+  for(int i = 1; i < argc; i++){
+
+    if(strcmp(argv[i], "-p") == 0){
+      if(i + 1 >= argc){
+        printf("ps: missing argument after -p\n");
+        exit(1);
+      }
+      pid_count = parse_pids(argv[++i], pids, 32);
+   } else if(strcmp(argv[i], "-o") == 0){
+      if(i + 1 >= argc){
+        printf("ps: missing argument after -p\n");
+        exit(1);
+      }
+      
+      char *colstr = argv[++i];
+      char *field = colstr;
+
+      while (num_cols < MAXCOLS && *field){
+        char *comma = strchr(field, ','); 
+
+        if (comma){
+          *comma = '\0';
+          columns[num_cols++] = field;
+          field = comma + 1;
+        } else {
+          columns[num_cols++] = field;
+          break;
+        }
+      }
+    } else {
+      printf("Usage: ps [-p pidlist] [-o col1[,col2,...]]\n");
+      exit(1);
+    } 
+  }
+
+  if(num_cols == 0){
+    columns[0] = "pid";
+    columns[1] = "state";
+    columns[2] = "name";
+    num_cols = 3;
+  }
+  
+  struct uproc up[64];
+  int n = ps(up, 64);
+  if (n < 0){
+    printf("ps: error in sys_ps\n");
+  }
+  
+  // print header
+  for(int i = 0; i < num_cols; i++){
+    printf("%s\t", columns[i]);
+  }
+  printf("\n");
+
+  // print process
+  for (int i = 0; i < n; i++){
+    if (pid_count > 0){
+      int match = 0;
+      for (int j = 0; j < pid_count; j++)  {
+        if(up[i].pid == pids[i]){
+          match = 1;
+          break;
+        }
+      }
+
+      if(!match){
+        continue; 
+      }
+    }
+
+    // Print each column in order.
+    for(int c = 0; c < num_cols; c++){
+      if(strcmp(columns[c], "pid") == 0){
+        printf("%d\t", up[i].pid);
+      } else if(strcmp(columns[c], "name") == 0){
+        printf("%s\t", up[i].name);
+      } else if(strcmp(columns[c], "state") == 0){
+        int s = up[i].state;
+        char *st = (s >= 0 && s < NELEM(states) && states[s]) ? states[s] : "???";
+        printf("%s\t", st);
+      } else {
+        printf("? \t");
+      }
+    }
+
+    printf("\n");
+  }
+  
   exit(0);
 }
