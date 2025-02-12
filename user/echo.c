e4e29344641791798dc09c9eb954076bd9bfff08
Noura Khajehnouri <noura3368@gmail.com>
2025-02-12 13:13:57 -0500

    adding switch statements and some comments

--- user/echo.c
+++ user/echo.c
@@ -9,27 +9,31 @@ void echo_function_enhanced(int argc, char *argv[], int start_val){
     int length_of_string = strlen(argv[i]);
     for(int j = 0; j < length_of_string + 1; j++){
       if(argv[i][j] == '\\'){
-        if(j + 1 != length_of_string && argv[i][j+1] == 'n'){
-          write(1, "\n", 1);
-          j += 1;
-        }
-        else if(j + 1 != length_of_string && argv[i][j+1] == 't'){
-          write(1, "\t", 1);
-          j += 1;
-        }
-        else {
-          write(1, &argv[i][j], 1);
+        if(j + 1 != length_of_string) {
+          switch (argv[i][j + 1]) {
+              case 'n':
+                  write(1, "\n", 1);
+                  j++; 
+                  break;
+              case 't':
+                  write(1, "\t", 1);
+                  j++; 
+                  break;
+              default:
+                  write(1, &argv[i][j], 1); // if \ just a part of the sentence, no escape sequences 
+                  break;
+          }
         }
       }
       else {
-        write(1, &argv[i][j], 1);   
+        write(1, &argv[i][j], 1); // input next character 
       }
     }
     if (i + 1 < argc){
-      write(1, " ", 1);
+      write(1, " ", 1); // put space between WORDS
     }
     else {
-      write(1, "\n", 1);
+      write(1, "\n", 1); // last character, so that next command can be inputted on next line 
     }
   }
 }
@@ -42,7 +46,8 @@ main(int argc, char *argv[])
     if(strcmp(argv[1], "-e") == 0){
       echo_function_enhanced(argc, argv, 2);
   }
-  else {
+  else { 
+    // this is original code for echo 
     for(i = 1; i < argc; i++){
       write(1, argv[i], strlen(argv[i]));
       if(i + 1 < argc){
