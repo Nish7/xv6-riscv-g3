8e99342f303c05d5c590b3389c6b10c508c3b7b3
Charzihan <guozihan156@gmail.com>
2025-02-13 21:58:45 -0500

    added help and version version to sleep

--- user/sleep.c
+++ user/sleep.c
@@ -2,6 +2,25 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+void print_help() {
+	fprintf(2, "Usage: sleep <TICKS>\n");
+	fprintf(2, "       sleep OPTION\n");
+	fprintf(2, "Pause for number of TICKS.\n");
+	fprintf(2, "TICKS must be an integer or is interpreted as integer if it is float.\n");
+	fprintf(2, "Given two or more arguments, pause for the amount of time specified by\n");
+	fprintf(2, "the sum of their values.\n");
+	fprintf(2, "--help\n");
+	fprintf(2, "display help and exit\n");
+	fprintf(2, "--version\n");
+	fprintf(2, "display version number\n");
+	exit(0);
+}
+
+void print_version() {
+	fprintf(2, "Version 1.0\n");
+	exit(0);
+}
+
 int is_positive_int(const char *s) {
 	// Check negative
 	if (*s == '-') return 0;
@@ -12,8 +31,11 @@ int is_positive_int(const char *s) {
 	return 1;
 }
 
-
 int main(int argc, char *argv[]) {
+	// Check for help or version info
+	if (strcmp(argv[1], "--help") == 0) print_help();
+	else if (strcmp(argv[1], "--version") == 0) print_version();
+
 	// Sleep requires at least 2 arguments 
 	// with the first being sleep itself and the second being the sleep ticks
 	if(argc < 2){
