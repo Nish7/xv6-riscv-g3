d030cae4e839cbd876953880f6897e54827d032f
Charzihan <guozihan156@gmail.com>
2025-02-13 19:31:16 -0500

    adding multi-argument support and better error checking

--- user/sleep.c
+++ user/sleep.c
@@ -2,18 +2,39 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-  // Sleep requires 2 arguments with the first being sleep itself and the second being the sleep ticks
-  if(argc != 2){
-    fprintf(2, "missing operand. Usage: sleep [ticks]\n");
-    exit(1);
-  }
+int is_positive_int(const char *s) {
+	// Check negative
+	if (*s == '-') return 0;
+	// Check non-digit
+	for (; *s; s++) {
+		if (*s < '0' || *s > '9') return 0;
+	}
+	return 1;
+}
 
-  // Sleep for specified number of ticks
-  int ticks = atoi(argv[1]);
-  if (sleep(ticks) < 0) { // sys_sleep return -1 if failed
-    fprintf(2, "Sleep failed\n");
-    exit(1);
-  }
-  exit(0);
+
+int main(int argc, char *argv[]) {
+	// Sleep requires at least 2 arguments 
+	// with the first being sleep itself and the second being the sleep ticks
+	if(argc < 2){
+		fprintf(2, "missing operand. Usage: sleep <ticks>\n");
+		exit(1);
+	}
+
+	// If there are multiple tick arguments, they are added together
+	int total_ticks = 0;
+	for (int i = 1; i<argc; i++) {
+		if (!is_positive_int(argv[i])) {
+			fprintf(2, "Sleep: invalid ticks '%s'\n", argv[i]);
+			exit(1);
+		}
+		total_ticks += atoi(argv[i]);
+	}
+	
+	// Sleep for total_ticks amount of time
+	if (sleep(total_ticks) < 0) { // sys_sleep return -1 if failed
+		fprintf(2, "Sleep failed\n");
+		exit(1);
+	}
+	exit(0);
 }
