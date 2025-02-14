cc5c729c1aade11dd49a6fba21b2b5700ebe4019
Charzihan <guozihan156@gmail.com>
2025-02-13 23:51:08 -0500

    updating based on reviewed changes

--- user/sleep.c
+++ user/sleep.c
@@ -3,21 +3,21 @@
 #include "user/user.h"
 
 void print_help() {
-	fprintf(2, "Usage: sleep <TICKS>\n");
-	fprintf(2, "       sleep OPTION\n");
-	fprintf(2, "Pause for number of TICKS.\n");
-	fprintf(2, "TICKS must be an integer or is interpreted as integer if it is float.\n");
-	fprintf(2, "Given two or more arguments, pause for the amount of time specified by\n");
-	fprintf(2, "the sum of their values.\n");
-	fprintf(2, "--help\n");
-	fprintf(2, "display help and exit\n");
-	fprintf(2, "--version\n");
-	fprintf(2, "display version number\n");
+	fprintf(1, "Usage: sleep <TICKS>\n");
+	fprintf(1, "       sleep OPTION\n");
+	fprintf(1, "Pause for number of TICKS.\n");
+	fprintf(1, "TICKS must be an integer or is interpreted as integer if it is float.\n");
+	fprintf(1, "Given two or more arguments, pause for the amount of time specified by\n");
+	fprintf(1, "the sum of their values.\n");
+	fprintf(1, "--help\n");
+	fprintf(1, "display help and exit\n");
+	fprintf(1, "--version\n");
+	fprintf(1, "display version number\n");
 	exit(0);
 }
 
 void print_version() {
-	fprintf(2, "Version 1.0\n");
+	fprintf(1, "Version 1.0\n");
 	exit(0);
 }
 
@@ -32,16 +32,17 @@ int is_positive_int(const char *s) {
 }
 
 int main(int argc, char *argv[]) {
-	// Check for help or version info
-	if (strcmp(argv[1], "--help") == 0) print_help();
-	else if (strcmp(argv[1], "--version") == 0) print_version();
-
 	// Sleep requires at least 2 arguments 
 	// with the first being sleep itself and the second being the sleep ticks
 	if(argc < 2){
 		fprintf(2, "missing operand. Usage: sleep <ticks>\n");
 		exit(1);
 	}
+        
+	// Check for help or version command
+	if (strcmp(argv[1], "--help") == 0) print_help();
+        else if (strcmp(argv[1], "--version") == 0) print_version();
+        
 
 	// If there are multiple tick arguments, they are added together
 	int total_ticks = 0;
